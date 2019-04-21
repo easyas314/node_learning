8e7f9fefc4274f6539c7bb3aee26747b6e75e0b3
easyas314 <30437725+easyas314@users.noreply.github.com>
2019-04-20 21:39:36 -0400

    start squarespace specific parsing

--- tests.js
+++ tests.js
@@ -1,36 +1,8 @@
-// prototype
+var fs = require('fs');
+var path = require('path');
 
-function User(){
-    this.name = "";
-    this.life = 100;
-    this.giveLife = function giveLife(targetPlayer){
-        targetPlayer.life += 1;
-        console.log(this.name + " gave 1 life to " + targetPlayer.name);
-    };
-};
-
-var Brad = new User();
-var Beth = new User();
-Brad.name = "Brad";
-Beth.name = "Beth";
-
-Brad.giveLife(Beth);
-console.log("Brad: " + Brad.life);
-console.log("Beth: " + Beth.life);
-
-// good idea fairy strikes; "How 'bout hitting each other?"
-// use prototype to add functions or properties to an existing class
-User.prototype.uppercut = function uppercut(targetPlayer){
-    targetPlayer.life -= 3;
-    console.log(this.name + " uppercuttied " + targetPlayer.name);
-
-};
-
-Beth.uppercut(Brad);
-console.log("Brad life: " + Brad.life);
-console.log("Beth life: " + Beth.life);
-
-// another example of prototype use
-User.prototype.magic = 60;
-console.log("Brad magic: " + Brad.magic);
-console.log("Beth magic: " + Beth.magic);
+var aFile = '\home\local\\too\many\count.txt';
+var bFile = '/regua//latext/path/stuff.html';
+console.log(path.normalize(aFile));
+console.log(path.basename(bFile));
+console.log(path.extname(bFile));
